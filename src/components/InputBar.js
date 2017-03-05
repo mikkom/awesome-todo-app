aa697c726f24764c224fe8100ad318dc3e2abcd3
Mikko Matilainen <mikko.matilainen@gofore.com>
2017-03-05 10:21:41 +0200

    Make InputBar controlled component

--- src/components/InputBar.js
+++ src/components/InputBar.js
@@ -1,11 +1,26 @@
 import React, { Component } from 'react';
 
 class InputBar extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: ''
+    }
+  }
+
+  handleChange = e => {
+    this.setState({
+      value: e.target.value
+    });
+  }
+
   render() {
     return (
       <div className="form-group">
         <input
           className="form-control"
+          value={this.state.value}
+          onChange={this.handleChange}
         />
       </div>
     );
