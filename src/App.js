dcc3eb004246b37ee3d52bfe715bb3354a45daa4
Mikko Matilainen <mikko.matilainen@gofore.com>
2017-03-05 08:55:33 +0200

    Add Header component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
+import Header from './components/Header';
 import Todos from './todos/Todos';
 
 class App extends Component {
   render() {
     return (
-      <div>
+      <div className="container">
+        <Header />
         <Todos />
       </div>
     );
