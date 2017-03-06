df5c9deae149e94a76c0cad5c4a2bbc3bb7c4f79
Mikko Matilainen <mikko.matilainen@gofore.com>
2017-03-06 20:03:16 +0200

    Create Redux store and dummy reducer and hook up Redux DevTools

--- src/index.js
+++ src/index.js
@@ -1,12 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { applyMiddleware, compose, createStore } from 'redux';
+import { Provider } from 'react-redux';
 import { Router, browserHistory } from 'react-router';
 import 'bootstrap/dist/css/bootstrap.css';
 import routes from './routes';
+import reducer from './rootReducer';
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(
+  reducer,
+  composeEnhancers(applyMiddleware())
+);
 
 ReactDOM.render(
-  <Router history={browserHistory}>
-    {routes}
-  </Router>,
+  <Provider store={store}>
+    <Router history={browserHistory}>
+      {routes}
+    </Router>
+  </Provider>,
   document.getElementById('root')
 );
