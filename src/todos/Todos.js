fdc6a37cec2c4f1a3db73c507438b7e770d83d36
Mikko Matilainen <mikko.matilainen@gofore.com>
2017-03-06 19:06:26 +0200

    Use get from API

--- src/todos/Todos.js
+++ src/todos/Todos.js
@@ -1,32 +1,22 @@
 import React, { Component } from 'react';
 import Todo from './Todo';
 import InputBar from '../components/InputBar';
-
-let latestId = 1;
-
-const todos = [{
-  id: latestId++,
-  name: 'Do the laundry',
-  done: false
-}, {
-  id: latestId++,
-  name: 'Wash the dishes',
-  done: true
-}, {
-  id: latestId++,
-  name: 'Take out the dog',
-  done: false
-}];
+import * as api from './api';
 
 class Todos extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      todos
+      todos: []
     }
   }
 
+  componentDidMount() {
+    api.getTodos().then(todos => this.setState({ todos }));
+  }
+
   addTodo = name => {
+    /*
     const todo = {
       id: latestId++,
       name,
@@ -34,17 +24,22 @@ class Todos extends Component {
     };
     const todos = [...this.state.todos, todo];
     this.setState({ todos });
+    */
   }
 
   removeTodo = id => {
+    /*
     const todos = this.state.todos.filter(todo => todo.id !== id);
     this.setState({ todos });
+    */
   }
 
   toggleTodo = id => {
+    /*
     const todo = this.state.todos.find(todo => todo.id === id);
     todo.done = !todo.done;
     this.setState({ todos });
+    */
   }
 
   render() {
