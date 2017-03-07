7294ec901eeb057f83e0c6f3036f69f0eb871d24
Mikko Matilainen <mikko.matilainen@gofore.com>
2017-03-07 21:13:20 +0200

    Add and hook up redux-saga

--- src/index.js
+++ src/index.js
@@ -2,16 +2,24 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { applyMiddleware, compose, createStore } from 'redux';
 import { Provider } from 'react-redux';
+import createSagaMiddleware from 'redux-saga';
 import { Router, browserHistory } from 'react-router';
 import 'bootstrap/dist/css/bootstrap.css';
 import routes from './routes';
 import reducer from './rootReducer';
+import rootSaga from './rootSaga';
+
+const sagaMiddleware = createSagaMiddleware();
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const store = createStore(
   reducer,
-  composeEnhancers(applyMiddleware())
+  composeEnhancers(applyMiddleware(
+    sagaMiddleware
+  ))
 );
+
+sagaMiddleware.run(rootSaga);
 
 ReactDOM.render(
   <Provider store={store}>
