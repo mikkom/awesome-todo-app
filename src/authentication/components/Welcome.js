dac39f4158829ea376196025d570a31bb6d0954b
Juhani Tapaninen <juhani.tapaninen@gofore.com>
2017-03-08 17:36:32 +0200

    Connect Welcome to state.

--- src/authentication/components/Welcome.js
+++ src/authentication/components/Welcome.js
@@ -1,17 +1,26 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import { Link } from 'react-router'
 
 export class Welcome extends Component {
   render() {
+    const { authenticated } = this.props;
     return (
       <div>
         <p>Welcome to Todo App</p>
-        <Link to='/todos' className="btn btn btn-info">
-          Go to todo-list
-        </Link>
+        {
+          authenticated &&
+          <Link to='/todos' className="btn btn btn-info">
+            Go to todo-list
+          </Link>
+        }
       </div>
     )
   }
 }
 
-export default Welcome;
+const mapStateToProps = state => ({
+  authenticated: state.auth.authenticated
+});
+
+export default connect(mapStateToProps, {})(Welcome);
